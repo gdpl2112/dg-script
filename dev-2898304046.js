bf1d989dda3f165911f87587eb6cd1c57d3dd171
Kloping <3474006766@qq.com>
2023-08-22 18:42:05 +0800

    dev

--- dev-2898304046.js
+++ dev-2898304046.js
@@ -1,16 +1,16 @@
-if (context.getType() === "group") {
+if (utils.getType() === "group") {
   if (msg.startsWith("解析快手图集")) {
-        if (context.getType() === "group") {
+        if (utils.getType() === "group") {
             var reg = /(https?|http|ftp|file):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/g;
             var urls = msg.match(reg)
             if (urls !== null) {
                 context.send("正在解析...\n请稍等")
                 var u0 = encodeURI(urls[0]);
-                var arr = JSON.parse(context.requestGet("http://kloping.top/api/search/parseImgs?url=" + u0 + "&type=ks"))
+                var arr = JSON.parse(utils.requestGet("http://kloping.top/api/search/parseImgs?url=" + u0 + "&type=ks"))
                 var builder = context.forwardBuilder();
                 for (var i = 0; i < arr.length; i++) {
                     var e = arr[i];
-                    builder.add(context.getBot().getId(), "AI", context.uploadImage(e))
+                    builder.add(utils.getBot().getId(), "AI", context.uploadImage(e))
                 }
                 context.send(builder.build())
             } else {
@@ -27,7 +27,7 @@ if (context.getType() === "group") {
     }
     //================百度结束
     if (msg.startsWith("网易云热评")) {
-        var review = JSON.parse(context.requestGet("https://api.andeer.top/API/wangyi.php" + msg.substring(5)));
+        var review = JSON.parse(utils.requestGet("https://api.andeer.top/API/wangyi.php" + msg.substring(5)));
         context.send(review.data);
     }
     //=================网易云热评结束
@@ -38,19 +38,19 @@ if (context.getType() === "group") {
     if (msg.startsWith("发送私信")) {
         context.send("已发送")
         var message = msg.substring(4)
-        context.getSender().sendMessage(message)
+        utils.getSender().sendMessage(message)
     }
     //======================发送私信结束
     if (msg.startsWith("跟我说")) {
         context.send("ok")
         var repeat = msg.substring(2)
-        context.getSubject().sendMessage(repeat)
+        utils.getSubject().sendMessage(repeat)
     }
     //======================复述结束
     if (msg.startsWith("传话")) {
         context.send("正在发送...请稍后")
         var args = msg.split(" ")
-        var tar = context.getBot().getFriend(Number(args[1]));
+        var tar = utils.getBot().getFriend(Number(args[1]));
         tar.sendMessage(args[2])
         context.send("发送完成")
     }
@@ -58,13 +58,13 @@ if (context.getType() === "group") {
     if (msg.startsWith("群传话")) {
         context.send("正在发送...请稍后")
         var args = msg.split(" ")
-        var tar = context.getBot().getGroup(Number(args[1]));
+        var tar = utils.getBot().getGroup(Number(args[1]));
         tar.sendMessage(args[2])
         context.send("发送完成")
     }
     //=============================群传话结束
     if (msg == "clear") {
-        context.clear()
+        utils.clear()
         context.send("清除完成")
     }
     //==============================清除所有变量完成
@@ -72,10 +72,10 @@ if (context.getType() === "group") {
         var del = msg.substring(3)
         if (del.length == 0) {
             context.send("请输入变量名！")
-        } else if (context.get(del) == null) {
+        } else if (utils.get(del) == null) {
             context.send("未找到该变量！")
         } else {
-            context.del(del)
+            utils.del(del)
             context.send("删除完成！")
         }
     }
@@ -88,7 +88,7 @@ if (context.getType() === "group") {
         } else if (create[1].length == 0) {
             context.send("请输入变量值！")
         } else {
-            context.set(create[0], create[1])
+            utils.set(create[0], create[1])
             context.send("创建成功")
         }
     }
@@ -98,31 +98,31 @@ if (context.getType() === "group") {
         if (name.length == 0) {
             context.send(请输入变量名)
         } else {
-            var value = context.get(name)
+            var value = utils.get(name)
             context.send(name + " : " + value)
         }
     }
     //=====================================获取变量结束
-    var upm = context.get("upm")
+    var upm = utils.get("upm")
     if (upm !== null && msg == upm) {
         context.send(msg);
     } else {
-        context.set("upm", msg)
+        utils.set("upm", msg)
     }
     //====================================跟读结束
-    var get_state = context.get("state")
+    var get_state = utils.get("state")
     if (get_state !== null || get_state == true) {
         var message = context.msg
-        var constructionA = context.get("construction")
-        constructionA.add(context.getBot().getId(), "AI", message)
+        var constructionA = utils.get("construction")
+        constructionA.add(utils.getBot().getId(), "AI", message)
         if (msg == "完成") {
             context.send(construction.build())
-            context.set("state", false)
+            utils.set("state", false)
         }
     } else if (msg == "创建聊天记录") {
-        context.set("state", true)
+        utils.set("state", true)
         context.send("请输入消息")
         var construct = context.forwardBuilder()
-        var construction = context.set("construct", null)
+        var construction = utils.set("construct", null)
     }
 }
