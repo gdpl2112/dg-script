7948fcd780c00e83e3c156a6f0d317e683145545
Kloping <3474006766@qq.com>
2023-08-22 18:44:02 +0800

    fix

--- dev-2898304046.js
+++ dev-2898304046.js
@@ -1,23 +1,19 @@
-if (utils.getType() === "group") {
-  if (msg.startsWith("解析快手图集")) {
-        if (utils.getType() === "group") {
-            var reg = /(https?|http|ftp|file):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/g;
-            var urls = msg.match(reg)
-            if (urls !== null) {
-                context.send("正在解析...\n请稍等")
-                var u0 = encodeURI(urls[0]);
-                var arr = JSON.parse(utils.requestGet("http://kloping.top/api/search/parseImgs?url=" + u0 + "&type=ks"))
-                var builder = context.forwardBuilder();
-                for (var i = 0; i < arr.length; i++) {
-                    var e = arr[i];
-                    builder.add(utils.getBot().getId(), "AI", context.uploadImage(e))
-                }
-                context.send(builder.build())
-            } else {
-                context.send("未发现链接")
+if (context.getType() === "group") {
+    if (msg.startsWith("解析快手图集")) {
+        var reg = /(https?|http|ftp|file):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/g;
+        var urls = msg.match(reg)
+        if (urls !== null) {
+            context.send("正在解析...\n请稍等")
+            var u0 = encodeURI(urls[0]);
+            var arr = JSON.parse(utils.requestGet("http://kloping.top/api/search/parseImgs?url=" + u0 + "&type=ks"))
+            var builder = context.forwardBuilder();
+            for (var i = 0; i < arr.length; i++) {
+                var e = arr[i];
+                builder.add(utils.getBot().getId(), "AI", context.uploadImage(e))
             }
+            context.send(builder.build())
         } else {
-            context.send("命令仅适用群聊")
+            context.send("未发现链接")
         }
     }
     //====================解析结束
