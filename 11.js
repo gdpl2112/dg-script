897d64ddc0a3ec45381f758e2adf9ec55e8a15f4
memory <2898304046@qq.com>
2023-08-24 22:16:35 +0800

    tuitui

--- 11.js
+++ 11.js
@@ -4,8 +4,8 @@ if (context.getType() == "MemberMuteEvent") {
 
 if (context.getType() == "MemberUnmuteEvent") {
     var at = event.getMember().getId()
-    var atA = context.deSerialize("[mirai:at:at]")
-    event.getGroup().sendMessage(context.newPlainText(atA) + "被解除禁言了")
+    var atA = context.deSerialize("<at:" + at + ">")
+    event.getGroup().sendMessage(atA + "被解除禁言了")
 }
 //========================================获取禁言事件
 
@@ -13,4 +13,4 @@ if (context.getType() == "BotMuteEvent") {
     var bemute = context.getBot().getGroup(Number(868060057))
     bemute.sendMessage(context.newPlainText("Bot（" + context.getBot().getId() + "）在群“" + event.getGroup().getName() + "”（" + event.getGroup().getId() + "）中被禁言" + "\n禁言时长为：" + event.getDurationSeconds() + "秒" + "\n操作者为" + event.getGroup().getMember().getName() + "（" + event.getGroup().getMember().getId() + "）"))
 }
-//=========================================获取Bot被禁言，在群868060057中提示(不知道get什么)
+//=========================================获取Bot被禁言，在群868060057中提示
