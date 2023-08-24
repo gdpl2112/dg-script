641202b8b1c22432a0c8732c5549fbce69ba2594
memory <2898304046@qq.com>
2023-08-24 13:56:48 +0800

    改

--- dev-3474006766.js
+++ dev-3474006766.js
@@ -40,7 +40,7 @@ if (context.getType() == "group") {
 
 if (msg.startsWith("读")) {
     context.send("ok")
-    var repeat = msg.substring(2)
+    var repeat = msg.substring(1)
     context.getSubject().sendMessage(repeat)
 }
 //======================复述结束
