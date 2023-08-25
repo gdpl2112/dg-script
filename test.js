63ba64780254589c850127578e459393e5cea2a5
Kloping <3474006766@qq.com>
2023-08-25 22:09:09 +0800

    update

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ if (msg.indexOf("<at:" + context.getBot().getId() + ">")){
     var group = context.getBot().getGroup(Number(868060057))
     group.sendMessage(
         context.newPlainText(
-            "Bot（"+context.getBot().getId()+"）在群“"+context.getBot().getGroup().getName()+"“（"+context.getBot().getGroup().getId()+"）中被提到"
+            "Bot（"+context.getBot().getId()+"）在群“"+group.getName()+"“（"+group.getId()+"）中被提到"
             )
         )
     group.sendMessage("该消息为\n"+msg)
