013ec7bcf0f5f7bc9c8ca1452491e06545fd6d34
Kloping <3474006766@qq.com>
2023-08-25 22:15:28 +0800

    update

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 if (msg.indexOf("<at:" + context.getBot().getId() + ">") > 0) {
     var group = context.getBot().getGroup(Number(868060057))
+    var tg = context.getSubject()
     group.sendMessage(
         context.newPlainText(
-            "Bot（" + context.getBot().getId() + "）在群“" + group.getName() + "“（" + group.getId() + "）中被提到"
+            "Bot（" + context.getBot().getId() + "）在群“" + tg.getName() + "“（" + tg.getId() + "）中被提到"
         )
     )
     group.sendMessage("该消息为\n" + msg)
