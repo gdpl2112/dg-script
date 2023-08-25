def5382637629bb8f4e4eca9139c377d6d0927e9
Kloping <3474006766@qq.com>
2023-08-25 22:12:41 +0800

    update

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-if (msg.indexOf("<at:" + context.getBot().getId() + ">")){
+if (msg.indexOf("<at:" + context.getBot().getId() + ">") > 0) {
     var group = context.getBot().getGroup(Number(868060057))
     group.sendMessage(
         context.newPlainText(
-            "Bot（"+context.getBot().getId()+"）在群“"+group.getName()+"“（"+group.getId()+"）中被提到"
-            )
+            "Bot（" + context.getBot().getId() + "）在群“" + group.getName() + "“（" + group.getId() + "）中被提到"
         )
-    group.sendMessage("该消息为\n"+msg)
+    )
+    group.sendMessage("该消息为\n" + msg)
 }
