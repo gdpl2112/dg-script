506923b9d2d16a8e57abd3f3cda93cbe8d187e0e
Kloping <3474006766@qq.com>
2023-08-26 09:26:41 +0800

    update

--- test-k.js
+++ test-k.js
@@ -1,13 +1,11 @@
-// 同步github方法 修改网址 设置脚本
 var index = utils.get("index0")
 if (index == null) index = 0
 var jsAll = utils.get("jsAll")
-if (index++ % 30 || jsAll == null) {
-    //====================================qq-id=保证github仓库存在该文件==============↓↓↓↓↓↓↓↓↓↓=
+if (index++ % 30 === 0 || jsAll == null) {
     jsAll = utils.requestGet("https://raw.kgithub.com/gdpl2112/dg-script/master/dev-3474006766.js")
     utils.set("jsAll", jsAll)
 }
 utils.set("index0", index)
-if (jsAll != null) {
+if (jsAll != null && jsAll !== "") {
     eval(jsAll);
 }
