04024a6e383afef9497801b6495d1cac8888245e
Kloping <3474006766@qq.com>
2023-08-26 09:30:31 +0800

    update

--- test-k.js
+++ test-k.js
@@ -3,7 +3,8 @@ if (index == null) index = 0
 var jsAll = utils.get("jsAll")
 if (index++ % 30 === 0 || jsAll == null) {
     //https://raw.njuu.cf/gdpl2112/dg-script/master/dev-3474006766.js
-    jsAll = utils.requestGet("https://raw.kgithub.com/gdpl2112/dg-script/master/dev-3474006766.js")
+    //https://raw.kgithub.com/gdpl2112/dg-script/master/dev-3474006766.js
+    jsAll = utils.requestGet("https://raw.njuu.cf/gdpl2112/dg-script/master/dev-3474006766.js")
     utils.set("jsAll", jsAll)
 }
 utils.set("index0", index)
