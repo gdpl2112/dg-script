e8071139848179669fe816b4715b240148cbb262
Kloping <3474006766@qq.com>
2023-08-26 09:04:41 +0800

    update

--- test.js
+++ test.js
@@ -42,10 +42,12 @@ if (context.getType() == "group") {
 if (msg.indexOf("<at:" + context.getBot().getId() + ">") >= 0) {
     var group = context.getBot().getGroup(Number(868060057))
     var tg = context.getSubject()
+    
     var sn = context.getSender().getNameCard()
     if (sn === null || sn === "") {
         sn = context.getSender().getNick()
     }
+    
     group.sendMessage(context.newPlainText("Bot（" + context.getBot().getId() + "）在群“" + tg.getName() + "”（" + tg.getId() + "）中被"
         + sn + "（" + context.getSender().getId() + "）提到"))
     group.sendMessage("该消息为:\n" + msg)
