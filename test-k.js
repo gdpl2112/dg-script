24c4f9ef75930c528ce679d453581cfc94e0efc5
Kloping <3474006766@qq.com>
2023-08-26 10:08:14 +0800

    upload

--- test-k.js
+++ test-k.js
@@ -1,13 +1,26 @@
-var index = utils.get("index0")
-if (index == null) index = 0
-var jsAll = utils.get("jsAll")
-if (index++ % 30 === 0 || jsAll == null) {
-    //https://raw.njuu.cf/gdpl2112/dg-script/master/dev-3474006766.js
-    //https://raw.kgithub.com/gdpl2112/dg-script/master/dev-3474006766.js
-    jsAll = utils.requestGet("https://raw.njuu.cf/gdpl2112/dg-script/master/dev-3474006766.js")
-    utils.set("jsAll", jsAll)
+if (context.getType() == "group") {
+    if (msg == "reget") {
+        jsAll = utils.requestGet("https://raw.njuu.cf/gdpl2112/dg-script/master/dev-3474006766.js")
+        utils.clear()
+        utils.set("jsAll", jsAll)
+        context.send("reget ok: " + jsAll)
+    } else {
+        work()
+    }
 }
-utils.set("index0", index)
-if (jsAll != null && jsAll !== "") {
-    eval(jsAll);
+
+function work() {
+    var index = utils.get("index0")
+    if (index == null) index = 0
+    var jsAll = utils.get("jsAll")
+    if (index++ % 30 === 0 || jsAll == null) {
+        //https://raw.njuu.cf/gdpl2112/dg-script/master/dev-3474006766.js
+        //https://raw.kgithub.com/gdpl2112/dg-script/master/dev-3474006766.js
+        jsAll = utils.requestGet("https://raw.njuu.cf/gdpl2112/dg-script/master/dev-3474006766.js")
+        utils.set("jsAll", jsAll)
+    }
+    utils.set("index0", index)
+    if (jsAll != null && jsAll !== "") {
+        eval(jsAll);
+    }
 }
