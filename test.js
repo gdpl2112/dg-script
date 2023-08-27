e23468f44f2565a0691833e9b16172d0e9201c23
memory <2898304046@qq.com>
2023-08-27 23:19:58 +0800

    1

--- test.js
+++ test.js
@@ -1,13 +1,10 @@
-if (msg.indexOf("<at:" + context.getBot().getId() + ">") >= 0) {
-    var group = context.getBot().getGroup(Number(868060057))
-    var tg = context.getSubject()
-    var sn = context.getSender().getNameCard()
-    if (sn === null || sn === "") {
-        sn = context.getSender().getNick()
+if (context.getType() == "group") {
+    if (msg.startsWith("kick")) {
+        var kickA = msg.substring(4)
+        if (kickA == "<at:" + context.getSubject().getId()+">") {
+            var kickId = parseFloat(kickA)
+            context.getSubject().get(kickId).kick()
+        }
     }
-    group.sendMessage(context.newPlainText("Bot（" + context.getBot().getId() + "）在群“" + tg.getName() + "”（" + tg.getId() + "）中被"
-        + sn + "（" + context.getSender().getId() + "）提到"))
-    group.sendMessage(context.deSerialize(("该消息为:\n" + msg)))
 }
-//=======================================如果Bot被提到，则转发至群
-//=======================================if msg检测到图片 则将jpg转化为图片发出 但是报错
+//=====================无报错 但不实施
