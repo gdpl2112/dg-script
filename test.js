55ef2c6731c3446603a75c7985bb1ca1d58dcedf
memory <2898304046@qq.com>
2023-08-28 12:29:56 +0800

    1

--- test.js
+++ test.js
@@ -1,31 +1,20 @@
-//获取指定格式值 目前仅支持获取第一个出现的格式元素
-function getFormatValue(fk, inStr) {
-    var i1 = inStr.indexOf("<")
-    var i2 = inStr.indexOf(">")
-    if (i1 <= 0 || i2 <= 0) return null
-    var at0 = inStr.substring(i1 + 1, i2)
-    var args = at0.split(":")
-    if (args[0] !== fk) {
-        return null
-    } else return args[1]
-}
-
-//获取at格式值并返回Number 或 null
-function getAtId(inStr) {
-    var end = getFormatValue("at", inStr)
-    if (end !== null) return Number(end)
-    else return null
-}
-
-if (context.getType() == "group") {
-    if (msg.startsWith("kick")) {
-        var tid = getAtId(msg)
-        if (tid == null) {
-            context.send("未发现AT")
-        } else {
-            context.send("尝试kick")
-            context.getSubject().get(tid).kick("tips")
-        }
+var kick_state = utils.get("kicker_state")
+if (kick_state !== null || kick_state == true) {
+    var qunid2 = utils.get("qunid1")
+    var tid2 = utils.get("tid1")
+    var tips = msg
+    context.getSubject(qunid2).get(tid2).kick(tips)
+    utils.set("kicker_state", false)
+} else if (msg.startsWith("kick")) {
+    var tid = getAtId(msg)
+    var qunid = context.getSubject()
+    if (tid == null) {
+        context.send("未发现at")
+    } else {
+        utils.set("kicker_state", true)
+        utils.set("tid1", tid)
+        utils.set("qunid1",qunid)
+        context.send("请输入踢出原因")
     }
 }
-//=====================无报错 但不实施
+//============================================kick结束
