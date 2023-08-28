57467fa9501bb610eec27f4f85bf22d89aec6b0e
Kloping <3474006766@qq.com>
2023-08-28 14:47:48 +0800

    update

--- dev-3474006766.js
+++ dev-3474006766.js
@@ -86,6 +86,22 @@ function getAtId(inStr) {
     else return null
 }
 
+function getAllNumber(str, df) {
+    var ns = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    var out = ""
+    for (var i = 0; i < str.length; i++) {
+        var e = str[i]
+        if (ns.includes(e)) {
+            out = out + e
+        }
+    }
+    if (out.length > 0) {
+        return Number(out)
+    } else {
+        return df
+    }
+}
+
 if (context.getType() == "group" || context.getType() == "friend") {
     if (msg.startsWith("上传") || msg.startsWith("upload")) {
         var iid = getFormatValue("pic", msg)
@@ -99,3 +115,25 @@ if (context.getType() == "group" || context.getType() == "friend") {
         }
     }
 }
+
+
+if (context.getType() == "group") {
+    if (msg.startsWith(".禁言")) {
+        var qid = getAtId(msg)
+        if (qid == null) {
+            var b = getAllNumber(msg.replace(qid, ""), 1)
+            if (msg.endsWith("秒") || msg.endsWith("s") || msg.endsWith(" ")) {
+                context.getSubject().get(qid).mute(b)
+            } else if (msg.endsWith("分") || msg.endsWith("m")) {
+                var timeM = Number(b * 60)
+                context.getSubject().get(qid).mute(timeM)
+            } else if (msg.endsWith("小时") || msg.endsWith("h")) {
+                var timeH = Number(b * 3600)
+                context.getSubject().get(qid).mute(timeH)
+            }
+        } else {
+            context.send("未发现at")
+        }
+    }
+}
+//===============================================================禁言结束
