49b124ad4953736317aaf64b947f9736dfe079d2
Kloping <3474006766@qq.com>
2023-08-28 14:56:38 +0800

    fix

--- test-k.js
+++ test-k.js
@@ -1,7 +1,9 @@
 
 //https://raw.njuu.cf/gdpl2112/dg-script/master/dev-3474006766.js
 //https://raw.kgithub.com/gdpl2112/dg-script/master/dev-3474006766.js
+
 var url = "https://raw.njuu.cf/gdpl2112/dg-script/master/dev-3474006766.js"
+
 if (context.getType() == "group") {
     if (msg == "reget") {
         var jsAll = utils.requestGet(url)
