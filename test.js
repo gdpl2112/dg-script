afad562ba3f13a3daf407811a0bfb6c09d0364c5
Kloping <3474006766@qq.com>
2023-08-28 13:37:42 +0800

    update

--- test.js
+++ test.js
@@ -20,20 +20,16 @@ if (context.getType() == "group") {
         var timeA = msg.substring(3).trim()
         var time = timeA.split(" ")
         var qid = Number(time[0])
-        if (time[0].length !== 0) {
-            if (time[1].length !== 0) {
-                var b = getAllNumber(time[1], 1)
-                if (msg.endsWith("秒") || msg.endsWith("s") || msg.endsWith(" ")) {
-                    context.getSubject().get(qid).mute(b)
-                } else if (msg.endsWith("分") || msg.endsWith("m")) {
-                    var timeM = Number(b * 60)
-                    context.getSubject().get(qid).mute(timeM)
-                } else if (msg.endsWith("小时") || msg.endsWith("h")) {
-                    var timeH = Number(b * 3600)
-                    context.getSubject().get(qid).mute(timeH)
-                }
-            } else {
-                context.send("未检测到时长")
+        if (qid == null) {
+            var b = getAllNumber(time[1], 1)
+            if (msg.endsWith("秒") || msg.endsWith("s") || msg.endsWith(" ")) {
+                context.getSubject().get(qid).mute(b)
+            } else if (msg.endsWith("分") || msg.endsWith("m")) {
+                var timeM = Number(b * 60)
+                context.getSubject().get(qid).mute(timeM)
+            } else if (msg.endsWith("小时") || msg.endsWith("h")) {
+                var timeH = Number(b * 3600)
+                context.getSubject().get(qid).mute(timeH)
             }
         } else {
             context.send("未发现at")
