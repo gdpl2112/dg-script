87f3bd7a7489ebb73819dea3d7802f797bcf9c69
Kloping <3474006766@qq.com>
2023-09-03 01:00:19 +0800

    update

--- dev-3474006766.js
+++ dev-3474006766.js
@@ -130,5 +130,28 @@ if (context.getType() === "group" || context.getType() === "friend") {
             context.send("upload finish: " + out)
         }
     }
+    //上传结束
+    if (msg.startsWith("解析ks")) {
+        if (context.getType() === "group") {
+            var reg = /(https?|http|ftp|file):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/g;
+            var urls = msg.match(reg)
+            if (urls !== null) {
+                context.send("正在解析...\n请稍等")
+                var u0 = encodeURI(urls[0]);
+                var arr = JSON.parse(utils.requestGet("http://kloping.top/api/search/parseImgs?url=" + u0 + "&type=ks"))
+                var builder = context.forwardBuilder();
+                for (var i = 0; i < arr.length; i++) {
+                    var e = arr[i];
+                    builder.add(context.getBot().getId(), "AI", context.uploadImage(e))
+                }
+                context.send(builder.build())
+            } else {
+                context.send("未发现链接")
+            }
+        } else {
+            context.send("命令仅适用群聊")
+        }
+    }
+    //解析结束
 }
 //23/9/3
