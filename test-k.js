c4c434da83a85ef958ff640f4e6c2f7625a1deae
memory <2898304046@qq.com>
2023-09-14 21:22:50 +0800

    1

--- test-k.js
+++ test-k.js
@@ -1,54 +1,64 @@
-//dg-2898304046-player_state 玩家注册状态
-//dg-2898304046-player_name 玩家name
-//dg-2898304046-player_gender 玩家性别
-//dg-2898304046-player_strength 玩家数值
-
-
-if (msg == "cache") {
-    utils.requestGet("http://kloping.top/del?pwd=dg-2898304046-player_state&key=")
-    utils.requestGet("http://kloping.top/del?pwd=dg-2898304046-player_name&key=")
-    utils.requestGet("http://kloping.top/del?pwd=dg-2898304046-player_gender&key=")
-    utils.requestGet("http://kloping.top/del?pwd=dg-2898304046-player_strength&key=")
-    context.send("clear all")
-}
-
-
-if (msg.startsWith("注册")) {
-    var playerId = context.getSender().getId()
-    var player_state = utils.requestGet("http://kloping.top/get?pwd=dg-2898304046-player_state&key=" + playerId)
-    if (player_state == "true") {
-        context.send("您已注册，请发送个人信息查看")
+//检测API开关状态
+function get_api_state() {
+    var get_api = utils.get("api_state")
+    if (get_api = null) {
+        var api_now_state = utils.requestGet("http://kloping.top/get?pwd=dg-2898304046&key=api_state")
+        utils.set("api_state", api_now_state)
+        return get_api
     } else {
-        var player_regist = msg.substring(2).trim()
-        var player_regist_information = player_regist.split(" ")
-        var beginStrength = Math.floor(Math.random() * (100 - 10 + 1)) + 10
-        var player_name = utils.requestGet("http://kloping.top/put?pwd=dg-2898304046-player_name&key=" + playerId + "&value=" + player_regist_information[0])
-        var player_gender = utils.requestGet("http://kloping.top/put?pwd=dg-2898304046-player_gender&key=" + playerId + "&value=" + player_regist_information[1])
-        var player_beginStrength = utils.requestGet("http://kloping.top/put?pwd=dg-2898304046-player_strength&key=" + playerId + "&value=" + beginStrength)
-        context.send("注册成功！\n您的昵称为：" + player_regist_information[0] + "\n您的性别为：" + player_regist_information[1] + "\n您的战力为：" + beginStrength)
-        utils.requestGet("http://kloping.top/put?pwd=dg-2898304046-player_state&key=" + context.getSender().getId() + "&value=true")
+        return get_api
     }
 }
 
-if (msg == "打坐") {
-    var playerId = context.getSender().getId()
-    var getPlayerStrength = utils.requestGet("http://kloping.top/get?pwd=dg-2898304046-player_strength&key=" + playerId)
-    var deltaPlayerStrength = Math.floor(Math.random() * (200 - -100 + 1)) + -100
-    var endPlayerStrength = Number(getPlayerStrength) + Number(deltaPlayerStrength)
-    var playerStrength = utils.requestGet("http://kloping.top/put?pwd=dg-2898304046-player_strength&key=" + playerId + "&value=" + endPlayerStrength)
-    context.send("您增加了" + endPlayerStrength + "点战力")
+
+//--------------------------------------------------------------------------------------------------------------------------------------------
+if (context.getType() == "group" || context.getType() == "friend") {
+    if (context.getSender().getId() == 2898304046) {
+        var okv = msg.split(" ")
+        switch (okv[0]) {
+            case "关闭api":
+                if (get_api_state() == true || get_api_state() == null) {
+                    utils.requestGet("http://kloping.top/put?pwd=dg-2898304046&key=api_state&value=" + Boolean(false))
+                    context.send("正在关闭api...")
+                } else {
+                    context.send("已关闭api")
+                }
+                break
+            case "开启api":
+                if (get_api_state() == false || get_api_state() == null) {
+                    utils.requestGet("http://kloping.top/put?pwd=dg-2898304046&key=api_state&value=" + Boolean(true))
+                    context.send("正在开启api...")
+                } else {
+                    context.send("已开启api")
+                }
+                break
+        }
+    }
 }
 
-if (msg = "个人信息") {
-    var playerId = context.getSender().getId()
-    var subject = context.getSubject()
-    var state = utils.requestGet("http://kloping.top/geet?pwd=dg-2898304046-player_state&key=" + playerId)
-    if (state = "true") {
-        var name = utils.requestGet("http://kloping.top/get?pwd=dg-2898304046-player_name&key=" + playerId)
-        var gender = utils.requestGet("http://kloping.top/get?pwd=dg-2898304046-player_gender&key=" + playerId)
-        var strength = utils.requestGet("http://kloping.top/get?pwd=dg-2898304046-player_strength&key=" + playerId)
-        context.send("您的昵称：" + name + "\n您的性别：" + gender + "\n您的战力:" + strength)
-    } else {
-        context.send("请发送注册+（空格）+昵称+（空格）+性别 进行注册")
+if (context.getType() == "group" || context.getType() == "friend") {
+    if (get_api_state() == true) {
+        if (msg.startsWith("喜报")) {
+            context.send(context.uploadImage("https://api.andeer.top/API/img_xibao.php?data=" + msg.substring(2)));
+        }
+        //====================喜报结束
+
+        if (msg.startsWith("甘雨抱抱你")) {
+            var id = getAtId(msg)
+            context.send(context.uploadImage("https://api.andeer.top/API/img_love.php?qq=" + id));
+        }
+        //====================甘雨抱抱你结束
+
+        if (msg.startsWith("百度")) {
+            var end = encodeURI(msg.substring(2));
+            context.send("https://m.baidu.com/s?word=" + end);
+        }
+        //================百度结束
+
+        if (msg.startsWith("网易云热评")) {
+            var review = JSON.parse(context.requestGet("https://api.andeer.top/API/wangyi.php" + msg.substring(5)));
+            context.send(review.data);
+        }
     }
+    //=================网易云热评结束
 }
