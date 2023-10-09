6f75d7e03ffb73db675637d0cd142c43bade028d
Kloping <3474006766@qq.com>
2023-10-09 21:59:57 +0800

    update

--- dev-3474006766.js
+++ dev-3474006766.js
@@ -244,8 +244,10 @@ if (context.getType() === "group") {
             context.send(context.newPlainText(list.toString()))
         }
     } else if (msg.indexOf("<at:3474006766>") >= 0) {
-        var req = msg.replace("<at:3474006766>", "").replace(/<qr:-?\d+>/g, "").trim()
-        if (req.length > 2) context.send(utils.requestGet("http://kloping.top/api/ai?req=" + req + "&id=3474006766"))
+        if (context.getSubject().getId() != 852662621) {
+            var req = msg.replace("<at:3474006766>", "").replace(/<qr:-?\d+>/g, "").trim()
+            if (req.length > 2) context.send(utils.requestGet("http://kloping.top/api/ai?req=" + req + "&id=3474006766"))
+        }
     }
 }
 
@@ -275,4 +277,4 @@ if (context.getType() == "NudgeEvent") {
         if (randomNum(1, 5) == 1) event.getFrom().nudge().sendTo(event.getSubject());
     }
 }
-//23/10/9-fix
+//23/10/9-update
