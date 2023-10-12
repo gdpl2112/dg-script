8afe53c9d862fb1da6aa370919ba71c635ca59e4
Kloping <3474006766@qq.com>
2023-10-13 00:05:23 +0800

    update

--- env.js
+++ env.js
@@ -2,36 +2,30 @@ class Context {
     getBot();
     //发送消息
     send(str);
-
+    //消息构造者
     builder();
 
+    //转发消息构造者其添加函数为add(id,name,msg)
     forwardBuilder();
-
     getSender();
-
     getSubject();
-
     getMessageChainById(id);
-
     createMusicShare(kind, title, summer, jumUrl, picUrl, url);
-
     uploadImage(url);
 
+    //创造一个纯文本消息
     newPlainText(text);
-
     deSerialize(msg);
     //判断环境 常为 friend 或者 group
     getType();
 }
-
 class Utils {
     //获得指定密文数据返回字符串
     requestGet(url);
-
     requestPost(url, data);
-
     queryUrlFromId(id);
 
+    //将文本转为可发送的对象
     serialize(msg);
     //获取内存中的变量
     get(key);
@@ -44,13 +38,15 @@ class Utils {
     //列出内存在所有变量
     list();
 
+    //执行一句sql
     executeSql(sql);
 
+    //执行一句查询sql返回list
     executeSelectList(sql);
 
+    //执行一句查询sql返回一个对象
     executeSelectOne(sql);
 }
-
 //消息体
 var msg = "";
 var context = new Context();
