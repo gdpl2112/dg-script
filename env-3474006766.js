937b084f456ff5a772f5ee92d151ec6ce61a6e45
klopong <3474006766@qq.com>
2023-10-14 13:20:22 +0800

    update

--- env-3474006766.js
+++ env-3474006766.js
@@ -15,6 +15,7 @@ var urls = [
     "https://raw.njuu.cf/gdpl2112/dg-script/master/dev-3474006766.js",
     "https://raw.githubusercontent.com/gdpl2112/dg-script/master/dev-3474006766.js",
     "https://gh.api.99988866.xyz/https://raw.githubusercontent.com/gdpl2112/dg-script/master/dev-3474006766.js",
+    "https://ghproxy.com/https://raw.githubusercontent.com/gdpl2112/dg-script/master/dev-3474006766.js",
 ]
 var index0 = utils.get("index0")
 if (index0 == null) index0 = 0
@@ -23,6 +24,7 @@ var url = urls[index0]
 if (context.getType() === "group") {
     if (msg === "update") {
         index0++
+        utils.set("index0",index0)
         if (index0 >= urls.length) index0 = 0
         var jsAll = utils.requestGet(url)
         utils.clear()
