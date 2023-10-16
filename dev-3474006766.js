7857c8cd48a0390b148ec92226dd7c01c73c772e
Kloping <3474006766@qq.com>
2023-10-16 23:14:43 +0800

    update

--- dev-3474006766.js
+++ dev-3474006766.js
@@ -231,7 +231,7 @@ if (context.getType() === "group" || context.getType() === "friend") {
         var okv = msg.split(" ");
         var json1 = utils.requestGet("https://ovoa.cc/api/yuanshen.php?message=" + okv[2] + "&key=123456&figure=" + okv[1])
         var d0 = JSON.parse(json1)
-        context.send("<audio:http://kloping.top/api/mp32amr?url=" + d0.audiourl + ">")
+        context.send("<audio:http://kloping.top/api/mp32amr?url=" + d0.url + ">")
         //context.send("<audio:" + d0.audiourl + ">")
     }
     else if (msg.startsWith("ai:")) {
@@ -311,4 +311,4 @@ if (context.getType() == "NudgeEvent") {
         if (getRandomInt(1, 5) == 1) event.getFrom().nudge().sendTo(event.getSubject());
     }
 }
-//23/10/16-23.13
+//23/10/16-23.14
