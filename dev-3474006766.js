c4aafc1a7143c804cc376af19efb89bc9f5650f2
Kloping <3474006766@qq.com>
2023-10-22 22:49:36 +0800

    update

--- dev-3474006766.js
+++ dev-3474006766.js
@@ -366,30 +366,4 @@ if (context.getType() === "group" || context.getType() === "friend") {
         debugLog("发送成功! to " + context.getSender().getId())
     }
 }
-
-if (context.getType() == "NudgeEvent") {
-    var bid = event.getBot().getId()
-    if (event.getFrom() == event.getBot()) {
-        //主动戳出 不做处理
-    } else if (event.getTarget().getId() == bid) {
-        var r0 = utils.get("nc0")
-        if (r0 == null) r0 = 1; else if (r0 > 4) r0 = 1;
-        switch (r0) {
-            case 1:
-                event.getSubject().sendMessage(context.newPlainText("(T＿T)"))
-                break;
-            case 2:
-                event.getSubject().sendMessage(context.newPlainText("有事么?￣へ￣"))
-                break;
-            case 3:
-                event.getSubject().sendMessage(context.newPlainText("阿巴阿巴?(〃'▽'〃)"))
-                break;
-            case 4:
-                event.getSubject().sendMessage(context.newPlainText("(｡ŏ_ŏ)走开"))
-                break;
-        }
-        utils.set("nc0", r0 + 1)
-        if (getRandomInt(1, 5) == 1) event.getFrom().nudge().sendTo(event.getSubject());
-    }
-}
 //23/10/20-14.29
