6dd9b697830186fd94dadf6be267951875e8fdc3
Kloping <3474006766@qq.com>
2023-11-26 20:13:37 +0800

    update

--- dev-291841860.js
+++ dev-291841860.js
@@ -1,21 +1,4 @@
 if (context.getType() === "group") msg = msg.trim()
-//完善
-function getFormatValue(fk, inStr) {
-    var i1 = inStr.indexOf("<")
-    var i2 = inStr.indexOf(">")
-    if (i1 < 0 || i2 <= 0) return null
-    var format0 = inStr.substring(i1 + 1, i2)
-    var args = format0.split(":")
-    if (args[0] !== fk) {
-        if (i2 <= inStr.length) return getFormatValue(fk, inStr.substring(i2 + 1))
-        else return null
-    } else return args[1]
-}
-
-function getAtId(inStr) {
-    var end = getFormatValue("at", inStr)
-    if (end !== null) return Number(end); else return null
-}
 
 function getTextFromOcr(url) {
     var json = utils.requestGet("http://localhost/api/ocr?url=" + url)
@@ -25,27 +8,6 @@ function getTextFromOcr(url) {
 
 function isStartOrEndWith(msg, key) {
     return (msg.startsWith(key) || msg.endsWith(key))
-}
-
-function getImageUrlAll(msg) {
-    var iid = getFormatValue("pic", msg)
-    if (iid == null) {
-        var msgId = getFormatValue("qr", msg)
-        if (msgId === null) {
-            return null
-        } else {
-            var msgc = context.getMessageChainById(msgId)
-            var msgcs = utils.serialize(msgc)
-            iid = getFormatValue("pic", msgcs)
-            if (iid == null) {
-                return null
-            } else {
-                return utils.queryUrlFromId(iid)
-            }
-        }
-    } else {
-        return utils.queryUrlFromId(iid)
-    }
 }
 
 function sendToText(out) {
@@ -67,29 +29,12 @@ function sendToText(out) {
 }
 
 function debugLog(msg) {
-    context.getBot().getGroup(696516964).sendMessage(context.newPlainText(msg))
+    context.getBot().getGroup(759590727).sendMessage(context.newPlainText(msg))
 }
 
 if (context.getType() == "group") {
     var sid = context.getSender().getId();
     var tid = context.getSubject().getId();
-    // utils.executeSql("CREATE TABLE IF NOT EXISTS `msg_list` (`sid` BIGINT NOT NULL,`tid` BIGINT NOT NULL,`msg` VARCHAR(1024) NOT NULL,`time` VARCHAR(255) NOT NULL)")
-    utils.executeSql("INSERT INTO `msg_list` (`sid`, `tid`, `msg`, `time`) VALUES (" + sid + ", " + tid + ", '" + msg + "', '" + new Date().getTime() + "');")
-    if (tid == 868060057 || tid == 696516964) {
-        if (msg == "扫码帮助") {
-            context.send("不填就是群的可选，1为qun.qq.com，2为vip.qq.com，3为qzone.qq.com，4为huifu.qq.com，5为id.qq.com，6为docs.qq.com，7为connect.qq.com")
-        } else if (msg.startsWith("扫码登录")) {
-            var result0 = JSON.parse(utils.requestGet("http://api.wuxixindong.cn/api/qqrcode.php?type=" + msg.substring(4)))
-            utils.set(sid, result0.qrsig)
-            context.send("<pic:" + result0.url + ">")
-        } else if (msg == "完成" || msg == "ok") {
-            var qrsig = utils.get(sid)
-            if (qrsig != null) {
-                var result1 = JSON.parse(utils.requestGet("https://api.wuxixindong.cn/api/qqrcode.php?qrsig=" + qrsig))
-                context.send(result1.text + "\n" + JSON.stringify(result1.data))
-            }
-        }
-    }
     if (sid == 3474006766) {
         if (msg.startsWith("/")) {
             var okv = msg.split(" ");
@@ -150,12 +95,6 @@ if (context.getType() == "group") {
                         context.send("del state : " + kdelOut + "! key:" + okv[1])
                     }
                     break
-                case "/open":
-                    context.send(utils.executeSql("UPDATE 'mk' SET 'k'=0 WHERE `tid`=" + tid))
-                    break
-                case "/close":
-                    context.send(utils.executeSql("UPDATE 'mk' SET 'k'=1 WHERE `tid`=" + tid))
-                    break
                 case "/req-get":
                     if (okv.length !== 2) {
                         context.send("args size less 2")
@@ -183,15 +122,6 @@ if (context.getType() == "group") {
                     if (okv.length !== 2) context.send("args size less 2")
                     context.send(okv[1])
                     break
-                case "/sql":
-                    context.send(utils.executeSql(msg.substring(4)))
-                    break
-                case "/sqls":
-                    sendToText(utils.executeSelectList(msg.substring(5)))
-                    break
-                case "/sqlso":
-                    sendToText(utils.executeSelectOne(msg.substring(6)))
-                    break
                 case "/test":
                     break
             }
@@ -208,7 +138,7 @@ function getAiUrl() {
 }
 
 function ParseVideoOrGallery(result) {
-    if (result.code == 200) {
+    if (result.code === 200) {
         var data = result.data
         if (result.msg.indexOf("图集") >= 0) {
             var builder = context.builder();
@@ -239,7 +169,7 @@ function ParseVideoOrGallery(result) {
 }
 
 if (context.getType() === "group" || context.getType() === "friend") {
-    if (context.getSender().getId() == context.getBot().getId() || context.getSender().getId() == 2898304046) {
+    if (context.getSender().getId() === 3474006766 || context.getSender().getId() === 2898304046) {
         if (isStartOrEndWith(msg, "上传") || isStartOrEndWith(msg, "upload")) {
             var iurl = getImageUrlAll(msg)
             if (iurl != null) {
@@ -247,39 +177,17 @@ if (context.getType() === "group" || context.getType() === "friend") {
                 var out = utils.requestGet("http://localhost/transImg?type=url&url=" + iurl)
                 context.send("upload finish: " + out)
             } else context.send("未发现图片")
-        } else if (isStartOrEndWith(msg, "query")) {
-            var iurl = getImageUrlAll(msg)
-            if (iurl != null)
-                context.send(iurl)
-            else
-                context.send("未发现图片")
-        } else if (isStartOrEndWith(msg, "识别")) {
-            var iurl = getImageUrlAll(msg)
-            if (iurl != null) {
-                iurl = encodeURI(iurl)
-                var out = getTextFromOcr(iurl)
-                if (out != null) {
-                    context.send(out)
-                } else context.send("识别失败")
-            } else context.send("未发现图片")
         } else if (msg === "aiclear") {
             context.send(utils.requestGet(getAiUrl() + "/clear?id=3474006766"))
         }
     }
     var tid = context.getSubject().getId();
-    // utils.executeSql("CREATE TABLE IF NOT EXISTS `mk` (`tid` BIGINT NOT NULL,  `k` TINYINT NOT NULL DEFAULT '0')")
-    var k = utils.executeSelectOne("SELECT k FROM `mk` WHERE `tid`=" + tid)
-    // debugLog(tid + " out: " + k)
-    if (k === null) {
-        utils.executeSql("INSERT INTO `mk` (`tid`) VALUES (" + tid + ");")
-        k = {"k": 0}
-    }
-    if (k.k !== 0) throw SyntaxError("end")
     if (msg.indexOf("kuaishou") > 0) {
         var reg = /(https?|http|ftp|file):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/g;
         var urls = msg.match(reg)
         if (urls !== null) {
             var url = urls[0];
+            context.send("正在解析\n" + url)
             var result = JSON.parse(utils.requestGet("http://ovoa.cc/api/kuaishou.php?url=" + url))
             ParseVideoOrGallery(result)
         } else context.send("未发现链接")
@@ -288,6 +196,7 @@ if (context.getType() === "group" || context.getType() === "friend") {
         var urls = msg.match(reg)
         if (urls !== null) {
             var url = urls[0];
+            context.send("正在解析\n" + url)
             var result = JSON.parse(utils.requestGet("http://ovoa.cc/api/douyin.php?url=" + url))
             ParseVideoOrGallery(result)
         } else context.send("未发现链接")
@@ -310,18 +219,11 @@ if (context.getType() === "group" || context.getType() === "friend") {
             context.send(builder.build())
             context.send(context.forwardBuilder().add(context.getBot().getId(), "AI:", context.newPlainText("视频直链:" + result.data.video.url)).build())
         }
-    } else if (msg.startsWith("语音合成")) {
-        var okv = msg.split(" ");
-        var u0 = "http://ovoa.cc/api/yuanshen.php?message=" + okv[2] + "&key=123456&figure=" + okv[1];
-        var json1 = utils.requestGet(u0)
-        var d0 = JSON.parse(json1)
-        // context.send("<audio:http://localhost/api/mp32amr?url=" + d0.url + ">")
-        context.send("<audio:" + d0.url + ">")
     } else if (msg.startsWith("点歌")) {
         var name = msg.substring(2)
         var out = utils.requestGet("https://xiaoapi.cn/API/yy.php?type=qq&msg=" + name + "&n=1")
         var outs = out.split("\n")
-        context.send("<Music:QQMusic," + outs[1].substring(3) + "," + outs[2].substring(3) + ",http://47.100.93.243:34740/," + outs[0].substring(3) + "," + outs[3].substring(5) + ">")
+        context.send("<music:QQMusic," + outs[1].substring(3) + "," + outs[2].substring(3) + ",http://47.100.93.243:34740/," + outs[0].substring(3) + "," + outs[3].substring(5) + ">")
     } else if (msg.startsWith("ai:")) {
         sendToText(utils.requestGet(getAiUrl() + "?req=" + msg.substring(3) + "&id=3474006766"))
     } else if (msg.startsWith("gsai:")) {
@@ -337,7 +239,7 @@ if (context.getType() === "group" || context.getType() === "friend") {
             context.send(context.forwardBuilder()
                 .add(context.getBot().getId(), "AI:", context.uploadImage("https://api.anosu.top/img?sort=setu"))
                 .build())
-        }else{
+        } else {
             context.getSubject().sendMessage(context.forwardBuilder()
                 .add(context.getBot().getId(), "AI:", context.uploadImage("https://api.anosu.top/img?sort=setu"))
                 .build())
@@ -371,4 +273,4 @@ if (context.getType() == "NudgeEvent") {
         if (getRandomInt(1, 5) == 1) event.getFrom().nudge().sendTo(event.getSubject());
     }
 }
-//23/11/19
+//23/11/26
