b9ecfb0ebe04964b74f8db3ee7745e6406385c02
Kloping <3474006766@qq.com>
2025-08-04 21:30:36 +0800

    feat: 添加群聊和私聊戳一戳返回功能
    - 新增 js-189696825.js 文件实现戳一戳事件处理
    - 优化 js-2898304046.js 文件中的随机数生成函数

---
+++ js-189696825.js
@@ -0,0 +1,16 @@
+function onBotEvent(event, utils) {
+    if (event.class.getSimpleName() === "NudgeEvent") {
+        var bid = event.getBot().getId()
+        var tid = event.getTarget().getId()
+        var fid = event.getFrom().getId()
+        if (fid === bid) return;
+        if (tid === bid) {
+            var bot = event.getBot()
+            if (event.getSubject() instanceof Java.type('net.mamoe.mirai.contact.Group')) {
+                bot.executeAction("group_poke", "{\"user_id\": " + event.getFrom().getId() + ",\"group_id\": " + event.getSubject().getId() + "}");
+            } else {
+                bot.executeAction("friend_poke", "{\"user_id\": " + event.getFrom().getId() + "}");
+            }
+        }
+    }
+}
--- js-2898304046.js
+++ js-2898304046.js
@@ -1,13 +1,5 @@
-//getRandomNumber
 function getRandomNumber(minNum, maxNum) {
-    switch (arguments.length) {
-        case 1:
-            return parseInt(Math.random() * minNum + 1, 10);
-        case 2:
-            return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
-        default:
-            return 0;
-    }
+    return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10);
 }
 
 function onBotEvent(event, utils) {
